ae033c2c9bdd1720e3a6aaeb2b1c6b38b0afb5d7
froossv <srivaths311@gmail.com>
2019-02-26 00:22:14 +0530

    Working JSON Return for Server Up

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -5,14 +5,20 @@ import (
     "net/http"
     "encoding/json"
 )
-type Dummy struct {
-    status string
-    text string
+
+type Confirm struct {
+    Status string `json: status`
 }
 
 func Index(w http.ResponseWriter, r *http.Request){
-    test := Dummy{
-
+    fmt.Fprintln(w, "Server is Up")
+    status := Confirm{
+        Status: "Online",
+    }
+    w.Header().Set("Content-Type","application/json; charset=UTF-8")
+    w.WriteHeader(http.StatusOK)
+    if serr := json.NewEncoder(w).Encode(status); serr != nil{
+        panic(serr)
     }
 }
 
