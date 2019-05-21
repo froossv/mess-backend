59dd569d68558cad1c3dcdc809fe48fe250b5edf
Vathsan <srivaths311@gmail.com>
2019-05-21 17:57:15 +0530

    Return Menu

--- handlers/getMenu.go
+++ handlers/getMenu.go
@@ -3,53 +3,50 @@ package handlers
 import (
   "net/http"
   "encoding/json"
+  "time"
   "fmt"
-  //"time"
-  "github.com/lib/pq"
-  "os"
-  "database/sql"
 )
 
-func GetMenu(w http.ResponseWriter, r *http.Request){
-    //currentDate := time.Now()
-    //beforeDate := time.Now().AddDate(0,0,-1);
-    //todayMenu := Menu{}
-    //beforeMenu := Menu{}
-    status := Confirm{
-        Status: "Online",
-        Text: "All right!",
-    }
-    dbUrl := os.Getenv("DATABASE_URL")
-    psqlInfo,_ := pq.ParseURL(dbUrl)
-    psqlInfo += " sslmode=require"
-    db,erro := sql.Open("postgres",psqlInfo)
-    if erro != nil{
-        fmt.Printf("Error in Validating")
-        panic(erro)
-    }
-    defer db.Close()
-    errp := db.Ping()
-    if errp != nil{
-        fmt.Printf("Error in Connecting")
-        panic(errp)
-    }
-    //tquery := "SELECT * FROM menu WHERE day = '" + currentDate.Format("2006-01-02") + "';"
-    //err1 := db.QueryRow(tquery).Scan(&todayMenu.day,&todayMenu.bf1,&todayMenu.bf1c,&todayMenu.bf2,&todayMenu.bf2c,&todayMenu.lun1,&todayMenu.lun1c,&todayMenu.lun2,&todayMenu.lun2c,&todayMenu.din1,&todayMenu.din1c,&todayMenu.din2,&todayMenu.din2c)
-    //yquery := "SELECT * FROM menu WHERE day = '" + beforeDate.Format("2006-01-02") + "';"
-    //err2 := db.QueryRow(yquery).Scan(&beforeMenu.day,&beforeMenu.bf1,&beforeMenu.bf1c,&beforeMenu.bf2,&beforeMenu.bf2c,&beforeMenu.lun1,&beforeMenu.lun1c,&beforeMenu.lun2,&beforeMenu.lun2c,&beforeMenu.din1,&beforeMenu.din1c,&beforeMenu.din2,&beforeMenu.din2c)
-    //if (err1!=nil || err2!=nil){
-    //    status.Text = "Not Good"
-    //    panic(err1)
-    //}else{
-    //    fmt.Println(todayMenu)
-    //    fmt.Println(beforeMenu)
-    //}
+//0 -> Tomorrow's Menu
+//1 -> Today's Menu
 
-    statusJson,err := json.Marshal(status)
-    if err!=nil{
-        panic(err)
+func GetMenu(w http.ResponseWriter, r *http.Request){
+    db := GetDB()
+    menu := Menu{}
+    status := Confirm{
+        Status: "error",
+        Text: "",
     }
-    w.Header().Set("Content-Type","application/json")
-    w.WriteHeader(http.StatusOK)
-    w.Write(statusJson)
+    date := time.Now()
+
+    option := r.URL.Query()["day"][0]
+    fmt.Println(option)
+    switch option {
+        case "0":
+            date = time.Now()
+        case "1":
+            date = time.Now().AddDate(0,0,1)
+    }
+
+    query := "SELECT * FROM menu WHERE day = '" + date.Format("2006-01-02") + "';"
+    errr := db.QueryRow(query).Scan(&menu.Day,&menu.Bf1,&menu.Bf1c,&menu.Bf2,&menu.Bf2c,&menu.Lun1,&menu.Lun1c,&menu.Lun2,&menu.Lun2c,&menu.Din1,&menu.Din1c,&menu.Din2,&menu.Din2c)
+    if(errr == nil){
+        fmt.Println(menu)
+        menuJson,err := json.Marshal(menu)
+        if(err!=nil){
+            panic(err)
+        }
+        w.Header().Set("Content-Type","application/json")
+        w.WriteHeader(http.StatusOK)
+        w.Write(menuJson)
+    }else{
+        fmt.Println(errr)
+        statusJson,err := json.Marshal(status)
+        if err!=nil{
+            panic(err)
+        }
+        w.Header().Set("Content-Type","application/json")
+        w.WriteHeader(http.StatusOK)
+        w.Write(statusJson)
+    }
 }
