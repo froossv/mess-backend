444d0c04aa1379ce11900dab46cb7cca22f4c1ab
Vathsan <srivaths311@gmail.com>
2019-06-20 12:30:49 +0530

    Upsert Orders

--- handlers/postOrders.go
+++ handlers/postOrders.go
@@ -23,7 +23,8 @@ func PostOrders(w http.ResponseWriter, r *http.Request){
     db:= GetDB();
     //tableName := CreateTable()
     //fmt.Println(tableName)
-    _,erre := db.Exec("INSERT INTO orders VALUES ($1,$2,$3,$4,$5,$6,$7,$8);",date.AddDate(0,0,1).Format("2006-01-02"),order.Username,order.Bf1,order.Bf2,order.Lun1,order.Lun2,order.Din1,order.Din2)
+    query := "INSERT INTO orders VALUES ($1,$2,$3,$4,$5,$6,$7,$8) ON CONFLICT (regno) DO UPDATE set bf1 = excluded.bf1,bf2 = excluded.bf2,lun1 = excluded.lun1, lun2 = excluded.lun2, din1 = excluded.din1, din2 = excluded.din2;"
+    _,erre := db.Exec(query,date.AddDate(0,0,1).Format("2006-01-02"),order.Username,order.Bf1,order.Bf2,order.Lun1,order.Lun2,order.Din1,order.Din2)
     if(erre == nil){
         status.Status = "true"
     }else{
