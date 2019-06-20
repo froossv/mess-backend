759c7bb6775f6fa94a9ea89bdd8ecece662f7be7
Vathsan <srivaths311@gmail.com>
2019-06-20 12:13:55 +0530

    Upsert Menu 2

--- handlers/postMenu.go
+++ handlers/postMenu.go
@@ -18,7 +18,7 @@ func PostMenu(w http.ResponseWriter, r *http.Request){
     if err != nil{
         panic(err)
     }
-    query := "INSERT INTO menu VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13) ON CONFLICT (day) DO UPDATE set bf1 = excluded.$2, bf1c = excluded.$3, bf2 = excluded.$4, bf2c = excluded.$5, lun1 = excluded.$6,lun1c = excluded.$7, lun2 = excluded.$8, lun2c = excluded.$9, din1 = excluded.$10, din1c = excluded.$11, din2 = excluded.$12,din2c = excluded.$13;"
+    query := "INSERT INTO menu VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13) ON CONFLICT (day) DO UPDATE set bf1 = excluded.bf1, bf1c = excluded.bf1c, bf2 = excluded.bf2, bf2c = excluded.bf2c, lun1 = excluded.lun1,lun1c = excluded.lun1c, lun2 = excluded.lun2, lun2c = excluded.lun2c, din1 = excluded.din1, din1c = excluded.din1c, din2 = excluded.din2,din2c = excluded.din2c;"
     fmt.Println("Got these :",menu.Bf1,menu.Bf1c,menu.Bf2,menu.Bf2c,menu.Lun1,menu.Lun1c,menu.Lun2,menu.Lun2c,menu.Din1,menu.Din1c,menu.Din2,menu.Din2c)
     menu.Day = currentTime.Format("2006-01-02")
     db := GetDB()
