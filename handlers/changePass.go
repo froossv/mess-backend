34e6f5531392a68e55899da2d35965532a95cb46
Vathsan <srivaths311@gmail.com>
2019-06-25 14:20:28 +0530

    Added Change Password

--- handlers/changePass.go
+++ handlers/changePass.go
@@ -1,11 +1,31 @@
 package handlers
 
 import(
-    //"fmt"
-    //"encoding/json"
+    "encoding/json"
     "net/http"
 )
 
 func ChangePass(w http.ResponseWriter, r *http.Request){
+    passwd := pass{}
+    db := GetDB()
+    status := Confirm{
+        Status: "true",
+        Text: "",
+    }
 
+    err := json.NewDecoder(r.Body).Decode(&passwd)
+    if err!= nil{
+        panic(err)
+    }
+    _,erra := db.Exec("UPDATE users SET pwd = $1 WHERE reg = $2",passwd.New,passwd.Username)
+    if erra!=nil{
+        status.Status = "false"
+    }
+    statusJson,errj := json.Marshal(status)
+    if errj!=nil{
+        panic(errj)
+    }
+    w.Header().Set("Content-Type","application/json")
+    w.WriteHeader(http.StatusOK)
+    w.Write(statusJson)
 }
--- handlers/forgotPass.go
+++ handlers/forgotPass.go
@@ -1,31 +1,11 @@
 package handlers
 
 import(
-    "encoding/json"
+    //"fmt"
+    //"encoding/json"
     "net/http"
 )
 
 func ForgotPass(w http.ResponseWriter, r *http.Request){
-    passwd := pass{}
-    db := GetDB()
-    status := Confirm{
-        Status: "true",
-        Text: "",
-    }
 
-    err := json.NewDecoder(r.Body).Decode(&passwd)
-    if err!= nil{
-        panic(err)
-    }
-    _,erra := db.Exec("UPDATE users SET pwd = $1 WHERE reg = $2",passwd.New,passwd.Username)
-    if erra!=nil{
-        status.Status = "false"
-    }
-    statusJson,errj := json.Marshal(status)
-    if errj!=nil{
-        panic(errj)
-    }
-    w.Header().Set("Content-Type","application/json")
-    w.WriteHeader(http.StatusOK)
-    w.Write(statusJson)
 }
