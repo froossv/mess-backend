9dbcbc1706f56c3748965f518f067744499f2924
Vathsan <srivaths311@gmail.com>
2019-06-30 15:53:13 +0530

    postorders complete

--- handlers/postOrders.go
+++ handlers/postOrders.go
@@ -17,20 +17,20 @@ func PostOrders(w http.ResponseWriter, r *http.Request){
     }
     var costs [6]int64
     var cost int64
+    var codes [6]string
     err := json.NewDecoder(r.Body).Decode(&order)
     if(err!=nil){
         panic(err)
     }
     fmt.Println("Got these:",order)
-
     db:= GetDB();
 
-    errw := db.QueryRow("SELECT bf1c,bf2c,lun1c,lun2c,din1c,din2c FROM menu WHERE day = $1;",date.Format("2006-01-02")).Scan(&costs[0],&costs[1],&costs[2],&costs[3],&costs[4],&costs[5])
+    errw := db.QueryRow("SELECT bf1c,bf2c,lun1c,lun2c,din1c,din2c FROM menu WHERE day = $1;",date.AddDate(0,0,1).Format("2006-01-02")).Scan(&costs[0],&costs[1],&costs[2],&costs[3],&costs[4],&costs[5])
     if errw != nil{
         fmt.Println(errw)
     }
     val := reflect.ValueOf(order)
-    for i := 1; i < val.NumField()-1; i++ {
+    for i := 1; i < val.NumField(); i++ {
         cost = cost + val.Field(i).Int() * costs[i-1]
     }
     query := "INSERT INTO orders VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9) ON CONFLICT (regno) DO UPDATE set bf1 = excluded.bf1,bf2 = excluded.bf2,lun1 = excluded.lun1, lun2 = excluded.lun2, din1 = excluded.din1, din2 = excluded.din2;"
@@ -40,7 +40,19 @@ func PostOrders(w http.ResponseWriter, r *http.Request){
     }else{
         fmt.Println(erre)
     }
-
+    for i := 1; i < val.NumField(); i++ {
+        if(val.Field(i).Int() == 0){
+            codes[i-1] = "null"
+        }else{
+            codes[i-1] = GenRand(5) + fmt.Sprintf("%02d",val.Field(i))
+        }
+    }
+    _,errt := db.Exec("INSERT INTO order_codes_tomorrow values($1,$2,$3,$4,$5,$6,$7) ON CONFLICT (regno) DO UPDATE set bf1 = excluded.bf1,bf2 = excluded.bf2,lun1 = excluded.lun1,lun2 = excluded.lun2,din1 = excluded.din1,din2 = excluded.din2;",order.Username,codes[0],codes[1],codes[2],codes[3],codes[4],codes[5])
+    if(errt == nil){
+        status.Status = "true"
+    }else{
+        fmt.Println(errt)
+    }
     statusJson,errj := json.Marshal(status)
     if errj!=nil{
         panic(errj)
