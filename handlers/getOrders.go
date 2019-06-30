e9bfce5737f7e2d2e2030330e314c77cb4d93276
Vathsan <srivaths311@gmail.com>
2019-06-30 19:00:04 +0530

    get orders done

--- handlers/getOrders.go
+++ handlers/getOrders.go
@@ -3,9 +3,40 @@ package handlers
 import (
     "fmt"
     "net/http"
-    //"encoding/json"
+    "encoding/json"
+    "strconv"
 )
 
+//0 -> today
+//0 -> tomorrow
 func GetOrders(w http.ResponseWriter, r *http.Request){
-    fmt.Fprintln(w, "postOrders is Up!")
+    db := GetDB()
+    defer db.Close()
+    codes := Code{}
+    var table string
+    option := r.URL.Query()["day"][0]
+    regno32,_ := strconv.ParseInt(r.URL.Query()["regno"][0],10,32)
+    regno := int(regno32)
+    switch option {
+        case "0":{
+            table = "order_codes_today"
+        }
+        case "1":{
+            table = "order_codes_tomorrow"
+        }
+    }
+    query := "SELECT bf1,bf2,lun1,lun2,din1,din2 FROM " + table + " WHERE regno = " + r.URL.Query()["regno"][0] +";"
+    erro := db.QueryRow(query).Scan(&codes.Bf1,&codes.Bf2,&codes.Lun1,&codes.Lun2,&codes.Din1,&codes.Din2)
+    if erro != nil{
+        fmt.Println(erro)
+    }
+    codes.Username = regno
+    codeJson,err := json.Marshal(codes)
+    if err!=nil{
+        panic(err)
+    }
+    w.Header().Set("Content-Type","application/json")
+    w.WriteHeader(http.StatusOK)
+    w.Write(codeJson)
+
 }
--- handlers/structs.go
+++ handlers/structs.go
@@ -47,12 +47,12 @@ type Menu struct{
 
 type Code struct{
     Username int `json: username`
-    bf1 string `json: bf1`
-    bf2 string `json: bf2`
-    lun1 string `json: lun1`
-    lun2 string `json: lun2`
-    din1 string `json: din1`
-    din2 string `json: din2`
+    Bf1 string `json: bf1`
+    Bf2 string `json: bf2`
+    Lun1 string `json: lun1`
+    Lun2 string `json: lun2`
+    Din1 string `json: din1`
+    Din2 string `json: din2`
 }
 
 type Confirm struct {
