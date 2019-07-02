d79d7a724b01539ffd4868eb1b905240c5401f71
Vathsan <srivaths311@gmail.com>
2019-07-02 11:51:02 +0530

    Print Query Temp 4

--- handlers/getOrders.go
+++ handlers/getOrders.go
@@ -19,19 +19,21 @@ func GetOrders(w http.ResponseWriter, r *http.Request){
         Din1: "null",
         Din2: "null",
     }
-    var table string = "dummy"
+    var name string = "dummy"
     option := r.URL.Query()["day"][0]
-    fmt.Sprintf("Orders option = %s",option)
+    fmt.Printf("Options  = %s",option)
     switch option {
         case "0":{
-            table = "order_codes_today"
+            fmt.Printf("Inside 0")
+            name = "order_codes_today"
         }
         case "1":{
-            table = "order_codes_tomorrow"
+            fmt.Printf("Inside 1")
+            name = "order_codes_tomorrow"
         }
     }
-    fmt.Println(table)
-    query := "SELECT bf1,bf2,lun1,lun2,din1,din2 FROM "+table+" WHERE regno = " + r.URL.Query()["regno"][0] +";"
+    fmt.Printf("Table name is %s",name)
+    query := "SELECT bf1,bf2,lun1,lun2,din1,din2 FROM "+ name +" WHERE regno = " + r.URL.Query()["regno"][0] +";"
     fmt.Println(query)
     erro := db.QueryRow(query).Scan(&codes.Bf1,&codes.Bf2,&codes.Lun1,&codes.Lun2,&codes.Din1,&codes.Din2)
     if erro != nil{
