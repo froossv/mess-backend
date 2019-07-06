a7c6079ede3b8c356987229b6e2775f09c5cc4e9
Vathsan <srivaths311@gmail.com>
2019-07-06 16:39:20 +0530

    Added Null Menu 2

--- handlers/getMenu.go
+++ handlers/getMenu.go
@@ -57,25 +57,16 @@ func GetMenu(w http.ResponseWriter, r *http.Request){
     }
     query := "SELECT * FROM menu WHERE day = '" + date.Format("2006-01-02") + "';"
     errr := db.QueryRow(query).Scan(&menu.Day,&menu.Bf1,&menu.Bf1c,&menu.Bf2,&menu.Bf2c,&menu.Lun1,&menu.Lun1c,&menu.Lun2,&menu.Lun2c,&menu.Din1,&menu.Din1c,&menu.Din2,&menu.Din2c)
-    if(errr == nil){
-        fmt.Println(menu)
-        menuJson,err := json.Marshal(menu)
-        if(err!=nil){
-            panic(err)
-        }
-        w.Header().Set("Content-Type","application/json")
-        w.WriteHeader(http.StatusOK)
-        w.Write(menuJson)
-    }else{
+    if(errr != nil){
         fmt.Println(errr)
-        statusJson,err := json.Marshal(status)
-        if err!=nil{
-            panic(err)
-        }
-        w.Header().Set("Content-Type","application/json")
-        w.WriteHeader(http.StatusOK)
-        w.Write(statusJson)
     }
+    menuJson,err := json.Marshal(menu)
+    if(err!=nil){
+        panic(err)
+    }
+    w.Header().Set("Content-Type","application/json")
+    w.WriteHeader(http.StatusOK)
+    w.Write(menuJson)
 }
 
 func returnMenu() string{
