8709a4250117314466fbe2bd1fd76ebd5374b522
Vathsan <srivaths311@gmail.com>
2019-11-10 17:12:05 +0530

    GetMenu Of Form IQA

--- handlers/getMenu.go
+++ handlers/getMenu.go
@@ -24,6 +24,18 @@ func GetMenu(w http.ResponseWriter, r *http.Request){
         Din: "null",
         Snk: "null",
     }
+    nm := Menu{
+        Bf: "null",
+        Lun: "null",
+        Din: "null",
+        Snk: "null",
+    }
+    no := Menu{
+        Bf: "null",
+        Lun: "null",
+        Din: "null",
+        Snk: "null",
+    }
     menu := SendMenu{
         TBf: "",
         TLun: "",
@@ -56,19 +68,19 @@ func GetMenu(w http.ResponseWriter, r *http.Request){
 
     date = time.Now().AddDate(0,0,1)
     querym = "SELECT bf,lun,din,snk FROM menu WHERE day = '" + date.Format("2006-01-02") + "';"
-    errm = db.QueryRow(querym).Scan(&tm.Bf,&tm.Lun,&tm.Din,&tm.Snk)
+    errm = db.QueryRow(querym).Scan(&nm.Bf,&nm.Lun,&nm.Din,&nm.Snk)
     queryo = "SELECT bf,lun,din,snk FROM orders WHERE day = '" + date.Format("2006-01-02") + "' AND reg = " + reg + ";"
-    erro = db.QueryRow(queryo).Scan(&to.Bf,&to.Lun,&to.Din,&to.Snk)
+    erro = db.QueryRow(queryo).Scan(&no.Bf,&no.Lun,&no.Din,&no.Snk)
     fmt.Println(tm)
     fmt.Println(to)
     if(errm != nil || erro != nil){
         fmt.Println(errm)
         fmt.Println(erro)
     }
-    writeToJson(&menu.NBf,tm.Bf,to.Bf)
-    writeToJson(&menu.NLun,tm.Lun,to.Lun)
-    writeToJson(&menu.NDin,tm.Din,to.Din)
-    writeToJson(&menu.NSnk,tm.Snk,to.Snk)
+    writeToJson(&menu.NBf,nm.Bf,no.Bf)
+    writeToJson(&menu.NLun,nm.Lun,no.Lun)
+    writeToJson(&menu.NDin,nm.Din,no.Din)
+    writeToJson(&menu.NSnk,nm.Snk,no.Snk)
 
     menuJson,err := json.Marshal(menu)
     if(err!=nil){
@@ -85,7 +97,10 @@ func writeToJson(Dest *string,menu,order string) string{
             if(order != "null"){
                 Bfos := strings.Split(order,",")
                 if(Bfos[i] != "null"){
-                    *Dest = *Dest + x + "-" + GenRand(5) + Bfos[i] + ","
+                    iqa := strings.Split(Bfos[i],"-")
+                    if(x == iqa[0]){
+                        *Dest = *Dest + x + "-" + GenRand(5) + iqa[1] + ","
+                    }
                 }else{
                     *Dest = *Dest + x + "-" + "null" + ","
                 }
